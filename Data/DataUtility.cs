9aa1fb9fe81da3836b86b936f6dfc355e845ef10
Anna Baschnagel <baschnagelac@gmail.com>
2023-02-27 17:39:17 -0500

    data utility

---
+++ Data/DataUtility.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using RecipeBook.Models;
+
+namespace RecipeBook.Data
+{
+    public class DataUtility
+    {
+        public static string GetConnectionString(IConfiguration configuration)
+        {
+            string? connectionString = configuration.GetConnectionString("DefaultConnection");
+            string? databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+            return string.IsNullOrEmpty(databaseUrl) ? connectionString! : BuildConnectionString(databaseUrl);
+
+            //if (string.IsNullOrEmpty(databaseUrl))
+            //    {
+            //        return connectionString
+            //    }
+            //    else
+            //    {
+            //        return BuildConnectionString(databaseUrl);
+            //    }
+            // comment
+        }
+
+        private static string BuildConnectionString(string databaseUrl)
+        {
+            var databaseUri = new Uri(databaseUrl);
+            var userInfo = databaseUri.UserInfo.Split(':');
+            var builder = new NpgsqlConnectionStringBuilder()
+            {
+                Host = databaseUri.Host,
+                Port = databaseUri.Port,
+                Username = userInfo[0],
+                Password = userInfo[1],
+                Database = databaseUri.LocalPath.TrimStart('/'),
+                SslMode = SslMode.Require,
+                TrustServerCertificate = true
+            };
+
+            return builder.ToString();
+        }
+
+        public static async Task ManagerDataAsync(IServiceProvider svcProvider)
+        {
+
+            //Obtaining the necessary services based on the IServiceProvider parameter
+            var dbContextSvc = svcProvider.GetRequiredService<ApplicationDbContext>();
+            var userManagerSvc = svcProvider.GetRequiredService<UserManager<RBUser>>();
+
+            //align the database by checking Migrations
+            await dbContextSvc.Database.MigrateAsync();
+
+
+
+        }
+    }
+}
